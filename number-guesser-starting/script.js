557c8d0ca7971a14cebd23a65133e4f0ccadae73
Francesco De Vivo <francescodvf@yahoo.it>
2023-12-01 12:55:59 +0100

    First upload Number Guesser JS Codecademy  Project

---
+++ number-guesser-starting/script.js
@@ -0,0 +1,48 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = (secret) => {
+   secret = Math.floor(Math.random() * 10)
+   return secret
+};
+
+console.log(generateTarget());
+
+const compareGuesses = (humanGuess, computerGuess, secret) => {
+  
+  const humanDiff = Math.abs(secret - humanGuess);
+  const computerDiff = Math.abs(secret - computerGuess);
+
+  if (humanDiff <= computerDiff) {
+    return true
+  } else {
+    return false
+  }
+
+};
+
+console.log(compareGuesses());
+
+const updateScore = winner => {
+  
+  if (winner === 'human') {
+    humanScore++
+  } else if (winner === 'computer') {
+    computerScore++  
+  }
+};
+
+console.log(updateScore());
+
+
+const advanceRound = () => {
+  currentRoundNumber++
+}
+
+//console.log(advanceRound());
+
+
+//console.log(generateTarget());
